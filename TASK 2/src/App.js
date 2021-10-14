fc29f2767b1e381b753a2a2be75770ad83b2e4b5
Dhruv-Paghdal <85886757+Dhruv-Paghdal@users.noreply.github.com>
2021-10-14 23:07:53 +0530

    Add files via upload

---
+++ TASK 2/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Button from './components/Button';
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <h1 className="text-center my-4">Our Valuable User's</h1>
+        <Button title="Get Users"/>
+      </div>
+    </>
+  );
+}
+
+export default App;
